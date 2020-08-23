3106ed56bc62f1a9180b27b78a3fa80623b11ec3
Darren Behan <darrenbehan@hotmail.com>
2020-08-23 15:25:21 +1000

    change

--- config/config.js
+++ config/config.js
@@ -18,11 +18,7 @@ module.exports = {
     dialect: process.env.DB_MYSQL
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT
+    useEnvVariable: process.env.JAWSDB_URL,
+    dialect: process.env.DB_MYSQL
   }
 };
