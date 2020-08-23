2e5f0a743e0837910225bb6a2d320ede6321b132
Darren Behan <darrenbehan@hotmail.com>
2020-08-23 15:22:56 +1000

    change

--- config/config.js
+++ config/config.js
@@ -22,7 +22,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: process.env.PORT,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT
   }
 };
