f42e9ea8bb83835778d4653f631a9f5cb0e3f289
RE000 <re.paterson000@gmail.com>
2020-09-02 19:28:57 +1000

    sorted double quote error

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const passport = require("./config/passport");
 const {
   allowInsecurePrototypeAccess
 } = require("@handlebars/allow-prototype-access");
-const compression = require('compression')
+const compression = require("compression");
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
